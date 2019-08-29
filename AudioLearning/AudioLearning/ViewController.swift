4aa58a3fc59922b3ef2f3cf42fc4726b0122075d
Han <csh771355@gmail.com>
2019-08-29 10:09:38 +1000

    1. add git ignore file 2. create an AudioLearning Xcode project 3. add swiftlint 4. add swiftlint.yml

---
+++ AudioLearning/AudioLearning/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  AudioLearning
+//
+//  Created by Han Chen on 2019/8/29.
+//  Copyright © 2019 cshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}

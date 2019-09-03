a793832123a79771440ad0511d4038158dff3c06
Han <csh771355@gmail.com>
2019-09-03 15:38:16 +1000

    1. provide portrait only 2. add EpisodeListViewController.swift and EpisodeDetailViewController.swift 3. add a navigation controller and two view controller on Main.storyboard

---
+++ AudioLearning/Views/EpisodeListViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EpisodeListViewController.swift
+//  AudioLearning
+//
+//  Created by Han Chen on 2019/9/3.
+//  Copyright © 2019 cshan. All rights reserved.
+//
+
+import UIKit
+
+class EpisodeListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

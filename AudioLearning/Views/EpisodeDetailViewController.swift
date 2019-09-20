59da3d24751bde45b9b87e51e2a410a9809d99c7
Han <csh771355@gmail.com>
2019-09-20 18:16:30 +1000

    add a feature that user can scroll up or down  the music player view

--- AudioLearning/Views/EpisodeDetailViewController.swift
+++ AudioLearning/Views/EpisodeDetailViewController.swift
@@ -15,10 +15,13 @@ class EpisodeDetailViewController: BaseViewController, StoryboardGettable {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var htmlTextView: UITextView!
     @IBOutlet weak var playerView: UIView!
+    @IBOutlet weak var playerViewHeight: NSLayoutConstraint!
     private let refreshControl = UIRefreshControl()
     
     var viewModel: EpisodeDetailViewModel!
     var musicPlayerViewController: MusicPlayerViewController!
+    private let maxPlayerViewHeight: CGFloat = 195.5
+    private let minPlayerViewHeight: CGFloat = 76
     private let disposeBag = DisposeBag()
 
     override func viewDidLoad() {
@@ -29,11 +32,16 @@ class EpisodeDetailViewController: BaseViewController, StoryboardGettable {
     
     private func setupUI() {
         automaticallyAdjustsScrollViewInsets = false
+        // playerView
         addChild(musicPlayerViewController)
         musicPlayerViewController.view.frame = playerView.bounds
         playerView.addSubview(musicPlayerViewController.view)
+        playerView.sendSubviewToBack(musicPlayerViewController.view)
+        addPanToPlayerView()
+        // refreshControl
         scrollView.isScrollEnabled = false
         scrollView.addSubview(refreshControl)
+        // htmlTextView
         htmlTextView.isEditable = false
         htmlTextView.isScrollEnabled = true
     }
@@ -78,5 +86,43 @@ class EpisodeDetailViewController: BaseViewController, StoryboardGettable {
             .disposed(by: disposeBag)
         
         viewModel.reload.onNext(())
+    }
+}
+
+// MARK: - Pan Player View Up & Down
+
+extension EpisodeDetailViewController {
+    
+    func addPanToPlayerView() {
+        let pan = UIPanGestureRecognizer(target: self, action: #selector(handlePanPlayerView))
+        playerView.addGestureRecognizer(pan)
+    }
+    
+    @objc func handlePanPlayerView(_ recognizer: UIPanGestureRecognizer) {
+        let translation = recognizer.translation(in: playerView)
+        let currentY = playerViewHeight.constant
+        var finalY = currentY - translation.y
+        
+        switch recognizer.state {
+        case .changed:
+            if finalY > maxPlayerViewHeight { finalY = maxPlayerViewHeight }
+            if finalY < minPlayerViewHeight { finalY = minPlayerViewHeight }
+            playerViewHeight.constant = finalY
+            playerView.superview?.layoutIfNeeded()
+        case .ended:
+            let distanceFromMax = abs(maxPlayerViewHeight - finalY)
+            let distanceFromMin = abs(minPlayerViewHeight - finalY)
+            if distanceFromMax > distanceFromMin {
+                finalY = minPlayerViewHeight
+            } else {
+                finalY = maxPlayerViewHeight
+            }
+            UIView.animate(withDuration: 0.4) { [weak self] in
+                guard let `self` = self else { return }
+                self.playerViewHeight.constant = finalY
+                self.playerView.superview?.layoutIfNeeded()
+            }
+        default: break
+        }
     }
 }

ea39a30c8aaf1cb23533593f78b5de7a9a6379fa
Han <csh771355@gmail.com>
2020-02-07 21:54:13 +0800

    fix date displaying issue

--- AudioLearning/Utilities/Extensions/ExtensionString.swift
+++ AudioLearning/Utilities/Extensions/ExtensionString.swift
@@ -14,6 +14,7 @@ extension String {
     func toDate(dateFormat: String) -> Date? {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = dateFormat
+        dateFormatter.locale = Locale(identifier: "en")
         return dateFormatter.date(from: self)
     }
     
